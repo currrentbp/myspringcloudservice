e6b6765978cc6f0c4458a3785acbd029d586f7a4
baopan <baopan@aixuexi.com>
2020-03-15 23:37:54 +0800

    first

---
+++ src/test/java/com/currentbp/myspringcloudservice/MyspringcloudserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.currentbp.myspringcloudservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyspringcloudserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
